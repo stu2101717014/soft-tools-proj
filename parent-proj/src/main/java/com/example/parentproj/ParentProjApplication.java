cf04e047de2853ede512c58e114f9be40ee562f8
unknown <milen.m.hinkov@gmail.com>
2022-05-09 13:58:30 +0300

    initial commit

---
+++ parent-proj/src/main/java/com/example/parentproj/ParentProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.parentproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParentProjApplication.class, args);
+    }
+
+}
---
+++ parent-proj/src/test/java/com/example/parentproj/ParoentProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parentproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
